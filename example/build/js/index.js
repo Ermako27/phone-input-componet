52f524f7cc03a935ddda9baa690508e6a91f3bbd
Ermako27 <ermakov.1828@gmail.com>
2019-07-23 00:46:41 +0300

    new example build

--- example/build/js/index.js
+++ example/build/js/index.js
@@ -1,6 +1,181 @@
-var e=new class{createElement(e){const t=document.createElement("div");t.className="phone-block__input";const n=document.createElement("input");return n.type="text",n.className=e.style,"disabled"==e.state?(n.disabled=!0,n.value=e.value):(n.placeholder="_",n.maxLength=1),t.appendChild(n),t}};var t=new class{createElement(e){const t=document.createElement("span");return t.className=e.style,t.innerText=e.value,t}};const n={I:{style:"phone-block__input_normal",value:"",state:"normal",element:"input"},X:{style:"phone-block__input_disabled",value:"X",state:"disabled",element:"input"},"*":{style:"phone-block__input_disabled",value:"●",state:"disabled",element:"input"}};function r(e,t=!1){const r=function(e){return Array.from(e).reduce((e,t)=>e+=" "!==t?t:"","")}(e);if(function(e){return ""===e}(r))throw Error("Your mask is empty");return n.I.style=t?"phone-block__input_error":"phone-block__input_normal",n.I.state=t?"error":"normal",Array.from(r).reduce((e,t)=>{if(t in n){const r=Object.assign({},n[t]);return e.push(r),e}if(/[0-9]/.test(t)){return e.push({value:t,style:"phone-block__input_disabled",state:"disabled",element:"input"}),e}return e.push({value:t,style:"phone-block__symbol-span",element:"span"}),e},[])}class PhoneComponent{constructor(e,t={error:!1}){this.mask=e,this.state=t;}setState({error:e=!1}){if(e!==this.state.error){const t=e?"phone-block__input_error":"phone-block__input_normal";document.querySelectorAll(`.phone-block__input > .${e?"phone-block__input_normal":"phone-block__input_error"}`).forEach(e=>{e.className=t;});const n=document.querySelector(".phone-block__error-message");e?n.classList.remove("phone-block__error-message_hide"):n.classList.add("phone-block__error-message_hide"),this.state.error=e;}}getComponentValue(){const e={maskNumber:"",rawNumber:""};let t=0;const n=document.querySelectorAll(`.phone-block__input > .${this.state.error?"phone-block__input_error":"phone-block__input_normal"}`);for(const r of this.mask)"I"===r?(e.maskNumber+=n[t].value.length?n[t].value:"I",e.rawNumber+=n[t].value,t+=1):e.maskNumber+=r;return e}createComponent(){const n=document.createElement("div"),o=document.createElement("div");o.className="phone-block",r(this.mask,this.state.error).forEach(n=>{if("input"===n.element){const t=e.createElement(n);o.appendChild(t);}else{const e=t.createElement(n);o.appendChild(e);}});const s=document.createElement("div");s.className="phone-block";const a=document.createElement("span");return a.classList.add("phone-block__error-message"),this.state.error||a.classList.add("phone-block__error-message_hide"),a.innerText="Неверный номер, попробуйте еще раз",s.appendChild(a),n.appendChild(o),n.appendChild(s),n}}
+class InputElement {
+    createElement(props) {
+        const div = document.createElement('div');
+        div.className = 'phone-block__input';
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.className = props.style;
+        if (props.state == 'disabled') {
+            input.disabled = true;
+            input.value = props.value;
+        }
+        else {
+            input.placeholder = '_';
+            input.maxLength = 1;
+        }
+        div.appendChild(input);
+        return div;
+    }
+}
+var InputElement$1 = new InputElement;
 
-// import PhoneComponent from 'phone-input-componet';
+class SpanElement {
+    createElement(props) {
+        const span = document.createElement('span');
+        span.className = props.style;
+        span.innerText = props.value;
+        return span;
+    }
+}
+var SpanElement$1 = new SpanElement;
+
+const maskLegend = {
+    'I': {
+        style: 'phone-block__input_normal',
+        value: '',
+        state: 'normal',
+        element: 'input',
+    },
+    'X': {
+        style: 'phone-block__input_disabled',
+        value: 'X',
+        state: 'disabled',
+        element: 'input',
+    },
+    '*': {
+        style: 'phone-block__input_disabled',
+        value: '●',
+        state: 'disabled',
+        element: 'input',
+    },
+};
+function removeSpaces(mask) {
+    return Array.from(mask).reduce((acc, elem) => {
+        acc += elem !== ' ' ? elem : '';
+        return acc;
+    }, '');
+}
+function isEmpty(mask) {
+    return mask === '';
+}
+function parseMask(mask, error = false) {
+    const maskWithoutSpaces = removeSpaces(mask);
+    if (isEmpty(maskWithoutSpaces)) {
+        throw new Error('Your mask is empty');
+    }
+    maskLegend['I'].style = error ?
+        'phone-block__input_error' :
+        'phone-block__input_normal';
+    maskLegend['I'].state = error ? 'error' : 'normal';
+    const result = Array.from(maskWithoutSpaces).reduce((acc, elem) => {
+        if (elem in maskLegend) {
+            const props = Object.assign({}, maskLegend[elem]);
+            acc.push(props);
+            return acc;
+        }
+        else if (/[0-9]/.test(elem)) {
+            const props = {
+                value: elem,
+                style: 'phone-block__input_disabled',
+                state: 'disabled',
+                element: 'input',
+            };
+            acc.push(props);
+            return acc;
+        }
+        else {
+            const props = {
+                value: elem,
+                style: 'phone-block__symbol-span',
+                element: 'span',
+            };
+            acc.push(props);
+            return acc;
+        }
+    }, []);
+    return result;
+}
+
+class PhoneComponent {
+    constructor(mask, state = { error: false }) {
+        this.mask = mask;
+        this.state = state;
+    }
+    setState({ error = false }) {
+        if (error !== this.state.error) {
+            const newInputStyle = error ?
+                'phone-block__input_error' :
+                'phone-block__input_normal';
+            const prevInputStyle = error ?
+                'phone-block__input_normal' :
+                'phone-block__input_error';
+            const elementsToChange = document
+                .querySelectorAll(`.phone-block__input > .${prevInputStyle}`);
+            elementsToChange.forEach((elem) => {
+                elem.className = newInputStyle;
+            });
+            const errorMsg = document.querySelector('.phone-block__error-message');
+            error ?
+                errorMsg.classList.remove('phone-block__error-message_hide') :
+                errorMsg.classList.add('phone-block__error-message_hide');
+            this.state.error = error;
+        }
+    }
+    getComponentValue() {
+        const result = {
+            maskNumber: '',
+            rawNumber: '',
+        };
+        let inputPointer = 0;
+        const inputStyle = this.state.error ?
+            'phone-block__input_error' :
+            'phone-block__input_normal';
+        const elementsToHarvest = document
+            .querySelectorAll(`.phone-block__input > .${inputStyle}`);
+        for (const elem of this.mask) {
+            if (elem === 'I') {
+                result.maskNumber +=
+                    elementsToHarvest[inputPointer].value.length ?
+                        elementsToHarvest[inputPointer].value :
+                        'I';
+                result.rawNumber += elementsToHarvest[inputPointer].value;
+                inputPointer += 1;
+            }
+            else {
+                result.maskNumber += elem;
+            }
+        }
+        return result;
+    }
+    createComponent() {
+        const container = document.createElement('div');
+        const phoneBlock = document.createElement('div');
+        phoneBlock.className = 'phone-block';
+        const arrayOfElementsProps = parseMask(this.mask, this.state.error);
+        arrayOfElementsProps.forEach((props) => {
+            if (props.element === 'input') {
+                const input = InputElement$1.createElement(props);
+                phoneBlock.appendChild(input);
+            }
+            else {
+                const span = SpanElement$1.createElement(props);
+                phoneBlock.appendChild(span);
+            }
+        });
+        const errorMsg = document.createElement('div');
+        errorMsg.className = 'phone-block';
+        const errorMsgSpan = document.createElement('span');
+        errorMsgSpan.classList.add('phone-block__error-message');
+        if (!this.state.error) {
+            errorMsgSpan.classList.add('phone-block__error-message_hide');
+        }
+        errorMsgSpan.innerText = 'Неверный номер, попробуйте еще раз';
+        errorMsg.appendChild(errorMsgSpan);
+        container.appendChild(phoneBlock);
+        container.appendChild(errorMsg);
+        return container;
+    }
+}
 
 let toggler = true;
 const mask = '+7(985)0II-**-**';
